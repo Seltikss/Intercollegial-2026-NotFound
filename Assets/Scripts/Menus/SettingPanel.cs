8886e5591e3d8ebd0f18fde83b62a08b2495de1c
Greg G. <greg.gionet@gmail.com>
2026-01-15 18:38:07 -0500

    Settings de controles

--- Assets/Scripts/Menus/SettingPanel.cs
+++ Assets/Scripts/Menus/SettingPanel.cs
@@ -1,4 +1,6 @@
 using System;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,29 +10,49 @@ public class SettingPanel : MonoBehaviour
     public InputActionReference shootInput;
     public InputActionReference reloadInput;
     public InputActionReference interactInput;
+    public TMP_Text dashText;
+    public TMP_Text shootText;
+    public TMP_Text reloadText;
+    public TMP_Text interactText;
     [SerializeField] private GameObject SettingMenu;
 
+    private void Update()
+    {
+        dashText.text = dashInput.action.GetBindingDisplayString();
+        shootText.text = shootInput.action.GetBindingDisplayString();
+        reloadText.text = reloadInput.action.GetBindingDisplayString();
+        interactText.text = interactInput.action.GetBindingDisplayString();
+    }
     public void OnDashRebindClicked()
     {
         OnAction(dashInput.action);
+        dashText.text = dashInput.action.GetBindingDisplayString();
+        dashText.text = dashInput.action.GetBindingDisplayString();
+
     }
 
 
     public void OnShootRebindClicked()
     {
         OnAction(shootInput.action);
+        shootText.text = shootInput.action.GetBindingDisplayString();
+        shootText.text = shootInput.action.GetBindingDisplayString();
     }
 
 
     public void OnReloadRebindClicked()
     {
         OnAction(reloadInput.action);
+        reloadText.text = reloadInput.action.GetBindingDisplayString();
+        reloadText.text = reloadInput.action.GetBindingDisplayString();
     }
 
 
     public void OnInteractRebindClicked()
     {
         OnAction(interactInput.action);
+        interactText.text = interactInput.action.GetBindingDisplayString();
+        interactText.text = interactInput.action.GetBindingDisplayString();
     }
 
     public void OnBackButtonClicked()
