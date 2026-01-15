0bd00517cdcd3e3cdf17215f13b293a6a59d41e3
Seltikss <80863341+Seltikss@users.noreply.github.com>
2026-01-15 09:38:43 -0500

    Separated sprite

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -1,9 +1,18 @@
+using System;
 using Player;
 using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private PlayerInput playerInput;
-    
-    
+
+
+    private void Update()
+    {
+        if (playerInput.moveVector.x != 0)
+            spriteRenderer.flipX = playerInput.moveVector.x < 0;
+        
+        
+    }
 }
