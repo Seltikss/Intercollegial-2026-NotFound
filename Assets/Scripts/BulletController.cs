c054faa2f260e8ba8f183a5a01a23b93b5e8de1c
Seltikss <80863341+Seltikss@users.noreply.github.com>
2026-01-15 15:17:23 -0500

    Fixer enemy and bullet bug

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -9,7 +9,10 @@ public class BulletController : MonoBehaviour
     [SerializeField] private Transform transform;
     [SerializeField] private SpriteRenderer spriteRenderer;
     
+    [SerializeField] private float c_collRadius = 1f;
+    
     private Vector2 direction = Vector2.zero;
+    public bool isEnabled = false;
 
 
     public void InitializeBullet(Vector2 dir)
@@ -20,29 +23,49 @@ public class BulletController : MonoBehaviour
         
         spriteRenderer.enabled = true;
         enabled = true;
+        isEnabled = true;
     }
 
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        Debug.Log(other.gameObject.name);
-        
-        switch (other.gameObject.tag)
-        {
-            case "Ground":
-                Destroy(other.gameObject);
-                break;
-            case "Enemy":
-                other.gameObject.GetComponent<EnemyHealth>().TakeDamage(1);
-                Destroy(other.gameObject);
-                break;
-        }
-    }
+    // private void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     Debug.Log(other.gameObject.name);
+    //     
+    //     switch (other.gameObject.tag)
+    //     {
+    //         case "Ground":
+    //             Destroy(other.gameObject);
+    //             break;
+    //         case "Enemy":
+    //             other.gameObject.GetComponent<EnemyHealth>().TakeDamage(1);
+    //             Destroy(other.gameObject);
+    //             break;
+    //     }
+    // }
 
 
     private void FixedUpdate()
     {
+        if (!isEnabled)
+            return;
+        
         Vector2 velocity = direction * SPEED * Time.fixedDeltaTime;
         transform.position += new Vector3(velocity.x, velocity.y, 0);
+
+        var results = Physics2D.OverlapCircleAll(transform.position, c_collRadius);
+        for (int i = 0; i < results.Length; i++)
+        {
+            Debug.Log(results[i].gameObject.tag);
+            switch (results[i].gameObject.tag)
+            {
+                case "Ground":
+                    Destroy(gameObject);
+                    break;
+                case "Enemy":
+                    results[i].gameObject.GetComponent<EnemyHealth>().TakeDamage(1);
+                    Destroy(gameObject);
+                    break;
+            }
+        }
     }
 }
