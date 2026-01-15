edaab62b1020578e70a160c1bd17a255e9db1dfb
Seltikss <80863341+Seltikss@users.noreply.github.com>
2026-01-15 10:53:08 -0500

    Player animation now show up

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.PlayerLoop;
+using Vector2 = UnityEngine.Vector2;
 
 namespace Player
 {
@@ -15,6 +17,8 @@ namespace Player
         [SerializeField] private InputActionReference interactInput;
 
         public Vector2 moveVector => moveInput.action.ReadValue<Vector2>();
+        public Vector2 nonZeroMoveVector = Vector2.right;
+        public Vector2 facingVector = Vector2.right;
         public bool isDashPressed { get; private set; } = false;
         public bool isShootPressed { get; private set; } = false;
         public bool isReloadPressed { get; private set; } = false;
@@ -25,10 +29,21 @@ namespace Player
         public bool isInteractJustPressed { get; private set; } = false;
         
         
+        public Vector2 GetMouseDirection()
+        {
+            Vector2 mouseDir = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+            mouseDir.Normalize();
+            return mouseDir;
+        }
 
 
         private void Update()
         {
+            if (moveVector != Vector2.zero)
+                nonZeroMoveVector = moveVector;
+
+            facingVector = GetMouseDirection();
+            
             isDashJustPressed = !isDashPressed && dashInput.action.IsPressed();
             isReloadJustPressed = !isReloadPressed && reloadInput.action.IsPressed();
             isInteractJustPressed = !isInteractPressed && interactInput.action.IsPressed();
