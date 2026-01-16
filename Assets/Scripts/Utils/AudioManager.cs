a2aaf8c48798e470ffb44b9552570c60f0bd02f3
Seltikss <80863341+Seltikss@users.noreply.github.com>
2026-01-15 21:45:29 -0500

    Created audio manager

---
+++ Assets/Scripts/Utils/AudioManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+namespace Utils
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public enum Musics
+        {
+            A,
+            B,
+            C,
+            D,
+        }
+        
+        public static AudioManager instance;
+
+        [SerializeField] private GameObject sourceInstance;
+        [SerializeField] private AudioSource musicPlayer;
+
+        private List<AudioSource> sources = new List<AudioSource>();
+
+        public AudioClip hurtPlayer;
+        public AudioClip deathPlayer;
+        
+
+        private void Start()
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+
+
+        public void Play(AudioClip clip, Transform t)
+        {
+            GameObject obj = Instantiate(sourceInstance, t);
+            AudioSource source = obj.GetComponent<AudioSource>(); 
+            sources.Add(source);
+            source.clip = clip;
+            source.Play();
+        }
+
+
+        public void PlayMusic(Musics index)
+        {
+            
+        }
+
+
+        private void Update()
+        {
+            for (int i = 0; i < sources.Count; i++)
+            {
+                if (!sources[i].isPlaying)
+                {
+                    Destroy(sources[i].gameObject);
+                    sources.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
