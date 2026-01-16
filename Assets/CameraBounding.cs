e7b0435c7a16bcb6f0cceac4549f7e4acba9af42
Adrian Baloi <adrianbaloi44@gmail.com>
2026-01-15 21:29:03 -0500

    camera fonctionnel

---
+++ Assets/CameraBounding.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraBounding : MonoBehaviour
+{
+    public Transform chamberTr;
+    private Camera cam;
+    private void Start()
+    {
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("asdf");
+        if (collision.CompareTag("Player"))
+            cam.transform.position = chamberTr.position + Vector3.back;
+    }
+}
